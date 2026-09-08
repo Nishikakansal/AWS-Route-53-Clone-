4ecb4f46a3ab4e23911f9bf29403726c38912b1f
nishika <nishikakansal@gmail.com>
2026-09-08 10:11:18 +0530

    database file update

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,12 +1,19 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./route53.db"
+
+DATABASE_PATH = os.getenv("DATABASE_PATH", "./route53.db")
+
+DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
+
 
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
